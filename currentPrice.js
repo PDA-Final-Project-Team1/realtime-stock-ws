10490b39e9980899ca126e2d7299ac0c36b156cf
DrivenByCode <jaykey1004@gmail.com>
2025-03-14 18:30:12 +0900

    refactor: 호가 및 체결가 반환 데이터 수정

--- currentPrice.js
+++ currentPrice.js
@@ -24,13 +24,11 @@ wss.on("connection", (ws) => {
       if (data.tr_id === "H0STCNT0" && data.tr_key) {
         console.log(`Subscribed to stock: ${data.tr_key}`);
 
-        // 랜덤 주가 데이터를 주기적으로 전송 (1초당 100개)
         const interval = setInterval(() => {
           const stockData = generateStockData(data.tr_key);
           ws.send(stockData);
-        }, 10); // 10ms마다 1개씩 전송 (100개/초)
+        }, 10);
 
-        // 클라이언트 연결 종료 시 setInterval 해제
         ws.on("close", () => {
           clearInterval(interval);
           console.log("Client disconnected");
@@ -42,18 +40,38 @@ wss.on("connection", (ws) => {
   });
 });
 
-// 랜덤 주가 데이터 생성 함수
 function generateStockData(stockCode) {
-  const price = (70000 + Math.random() * 2000).toFixed(0); // 70000~72000원 사이 랜덤 가격
-  const change = (Math.random() * 200 - 100).toFixed(0); // -100 ~ +100 랜덤 변동폭
-  const changeRate = ((change / price) * 100).toFixed(2); // 변동률 계산
   const timestamp = new Date();
   const hours = timestamp.getHours().toString().padStart(2, "0");
   const minutes = timestamp.getMinutes().toString().padStart(2, "0");
   const seconds = timestamp.getSeconds().toString().padStart(2, "0");
   const time = `${hours}${minutes}${seconds}`;
 
-  return `${stockCode}^${time}^${price}^5^${change}^${changeRate}`;
+  const price = (70000 + Math.random() * 2000).toFixed(0);
+  const change = (-900 + Math.random() * 200 - 100).toFixed(0);
+  const changeRate = ((change / price) * 100).toFixed(2);
+  const avgPrice = (price * (1 + Math.random() * 0.01)).toFixed(2);
+  const highPrice = (parseInt(price) + Math.floor(Math.random() * 300)).toFixed(
+    0
+  );
+  const lowPrice = (parseInt(price) - Math.floor(Math.random() * 300)).toFixed(
+    0
+  );
+  const volume = Math.floor(Math.random() * 15000000);
+  const tradeValue = volume * price;
+  const bidVolume = Math.floor(Math.random() * 70000);
+  const askVolume = Math.floor(Math.random() * 70000);
+  const netBuy = bidVolume - askVolume;
+  const foreignHold = (Math.random() * 100).toFixed(2);
+  const programBuy = Math.floor(Math.random() * 20000);
+  const programSell = Math.floor(Math.random() * 20000);
+  const programNetBuy = programBuy - programSell;
+  const expectedPrice = (
+    parseInt(price) + Math.floor(Math.random() * 50 - 25)
+  ).toFixed(0);
+  const totalTradeVolume = Math.floor(Math.random() * 20000000);
+
+  return `0|H0STCNT0|001|${stockCode}^${time}^${price}^5^${change}^${changeRate}^${avgPrice}^${price}^${highPrice}^${lowPrice}^${price}^${price}^2^${volume}^${tradeValue}^${bidVolume}^${askVolume}^${netBuy}^${foreignHold}^${programBuy}^${programSell}^5^0.42^63.09^090004^3^0^090706^5^-400^091435^2^100^20250224^31^N^130115^59740^1126598^2083435^0.23^${totalTradeVolume}^73.11^0^^${expectedPrice}`;
 }
 
 app.get("/", (req, res) => {
